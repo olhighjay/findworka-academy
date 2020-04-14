26b011862bf1f3d6f44cebdd8f69040060306273
Ifeoluwa Olajide <olhighjayartrage@gmail.com>
2020-04-14 17:37:05 +0100

    correction

--- database/migrations/2020_04_14_163402_create_payment_statuses_table.php
+++ database/migrations/2020_04_14_163402_create_payment_statuses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePaymentStatusTable extends Migration
+class CreatePaymentStatusesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePaymentStatusTable extends Migration
      */
     public function up()
     {
-        Schema::create('payment_status', function (Blueprint $table) {
+        Schema::create('payment_statuses', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreatePaymentStatusTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('payment_status');
+        Schema::dropIfExists('payment_statuses');
     }
 }
