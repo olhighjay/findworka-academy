b18b39cd1f0caba8611969e92efeb279a924235f
Ifeoluwa Olajide <olhighjayartrage@gmail.com>
2020-04-14 23:10:20 +0100

    correction

--- database/migrations/2020_04_14_220738_create_suspensions_table.php
+++ database/migrations/2020_04_14_220738_create_suspensions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRolesTable extends Migration
+class CreateSuspensionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('suspensions', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('display_name');
-            $table->string('description');
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ class CreateRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('suspensions');
     }
 }
