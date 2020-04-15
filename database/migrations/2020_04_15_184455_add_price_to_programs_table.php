d17d44bf3992fcf9d0db02f43490a9990812e0eb
Ifeoluwa Olajide <olhighjayartrage@gmail.com>
2020-04-15 19:59:26 +0100

    addition of price column to programs table

---
+++ database/migrations/2020_04_15_184455_add_price_to_programs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPriceToProgramsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('programs', function (Blueprint $table) {
+            $table->float('price')->default(0.00);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('programs', function (Blueprint $table) {
+            //
+        });
+    }
+}
