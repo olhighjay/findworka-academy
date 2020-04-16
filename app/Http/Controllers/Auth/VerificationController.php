2f503c31c577fa47ca02de22d3e6a12b5c6f581c
Ifeoluwa Olajide <olhighjayartrage@gmail.com>
2020-04-16 01:41:04 +0100

    correction

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -32,10 +32,10 @@ class VerificationController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-        $this->middleware('signed')->only('verify');
-        $this->middleware('throttle:6,1')->only('verify', 'resend');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    //     $this->middleware('signed')->only('verify');
+    //     $this->middleware('throttle:6,1')->only('verify', 'resend');
+    // }
 }
