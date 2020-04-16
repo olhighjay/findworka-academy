1e01c872d6a78fcbe876f0e333d11e0f187dd9c6
Ifeoluwa Olajide <olhighjayartrage@gmail.com>
2020-04-16 01:25:13 +0100

    correction

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -23,7 +23,7 @@ class DashboardController extends Controller
 
     public function __construct()
     {
-        $this->middleware(['auth', 'verified']);
+        $this->middleware(['auth',]);
     }
 
     /**
--- app/User.php
+++ app/User.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laratrust\Traits\LaratrustUserTrait;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable 
 {
     use LaratrustUserTrait;
     use Notifiable;
