65e023b58578cfaabb23aeb063b1ded231a89d6e
Ifeoluwa Olajide <olhighjayartrage@gmail.com>
2020-04-21 00:02:00 +0100

    correction

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 
 use App\Course;
--- app/Http/Controllers/TutorsDashboardController.php
+++ app/Http/Controllers/TutorsDashboardController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 use App\User;
 use App\Role;
